48e1e611d46b48c16815dc3b92a6ae8967ada9bd
Mithul Chawda <Mithul.Chawda@copart.com>
2020-08-27 23:59:03 +0530

    message - added Demo project to git local repo

---
+++ src/main/java/com/git/GitExampleApplication.java
@@ -0,0 +1,17 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExampleApplication.class, args);
+		
+		
+		System.out.println("this is git example");
+		
+	}
+
+}
